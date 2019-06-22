78c986980b8cc6c2163aa771b158ba2a688ab321
Mikhail Advani <mikhail.advani@gmail.com>
2019-06-22 19:07:10 +0200

    Part 2

---
+++ part-2/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  version = "2.16.0"
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_workshop" {
+  bucket = "terraform-workshop-mikhailadvani"
+}
+
+resource "local_file" "user" {
+    content = <<EOF
+Name: "abc"
+Email: "abc@email.com"
+EOF
+    filename = "${path.module}/user.txt"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "${aws_s3_bucket.terraform_workshop.bucket}"
+  key    = "${local_file.user.filename}"
+  source = "${path.module}/user.txt"
+}
